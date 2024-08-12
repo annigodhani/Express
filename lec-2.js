c064cc31c99f24b7024805e05cc5af418fc7ad4a
annigodhani <aniruddhgodhani@gmail.com>
2024-08-12 10:21:10 +0530

    12/08/24

--- lec-2.js
+++ lec-2.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const server = express();   // create server
-// const data = require('./friend.json');
-// const fs = require('fs');
-// const data = fs.readFileSync('./friend.json', 'utf8');
-// console.log(data);
+const express = require('express')
+const server = express()  // create server
+// const data = require('./friend.json')
+// const fs = require('fs')
+// const data = fs.readFileSync('./friend.json', 'utf8')
+// console.log(data)
 
-const myFun = (req, res, next) =>{
-    // console.log(req.query);
-    if(req.query.age >= 18){
-        console.log('Success');
-        next();
-    }else{
-        res.Json({message: "Sorry you are not allowed to visit this website ..."})
+const myFun = (req, res, next) => {
+    // console.log(req.query)
+    if (req.query.age >= 18) {
+        console.log('Success')
+        next()
+    } else {
+        res.Json({ message: "Sorry you are not allowed to visit this website ..." })
     }
 }
 
@@ -19,36 +19,33 @@ const myFun = (req, res, next) =>{
 
 // POST, GET PUT, PATCH, DELETE
 server.get('/', (req, res) => {
-    res.write('Welcome to Express');
-    res.end();
+    res.write('Welcome to Express')
+    res.end()
 })
 
 server.get('/login', myFun, (req, res) => {
-    res.write({msg:'Welcome to Login Page'});
-    res.end();
+    res.write({ msg: 'Welcome to Login Page' })
+    res.end()
 })
 
 server.post('/', (req, res) => {
-    // res.write('Welcome to Post Method');
-    res.send('Welcome to Post Method');
+    // res.write('Welcome to Post Method')
+    res.send('Welcome to Post Method')
 })
 
 // server.put('/', (req, res) => {
-//     res.json({msg:'Hello Put Method Called'});
+//     res.json({msg:'Hello Put Method Called'})
 // })
 
 // server.patch('/', (req, res) => {
-//     res.status(400);
-//     res.json({msg:'Hello Patch Method Called'});
+//     res.status(400)
+//     res.json({msg:'Hello Patch Method Called'})
 // })
 
 // server.get('/user', (req, res) => {
-//     res.json(JSON.parse(data));
+//     res.json(JSON.parse(data))
 // })
 
-
-
-
 server.listen(8000, () => {
-    console.log('Server Start at http://localhost8000');
-});
+    console.log('Server Start at http://localhost8000')
+})
--- server.js
+++ server.js
@@ -1,45 +1,67 @@
-const express = require('express')
-const server = express()   // create server
-// const data = require('./friend.json')
-const fs = require('fs')
-// const data = fs.readFileSync('./friend.json', 'utf8')
-// console.log(data)
 
-// POST, GET PUT, PATCH, DELETE
-server.get('/', (req, res) => {
-    res.write('Welcome to Express');
-    res.end();
+const express = require('express') 
+const morgan = require('morgan') 
+const app = express() 
+const users = require('./friend.json') 
+// console.log(users) 
+ 
+app.use(morgan('dev')) 
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: false })) 
+ 
+app.get('/', (req, res) => { 
+    res.send("Welcome To Express Server...") 
+}) 
+ 
+// CRUD  
+// Creat User  
+ 
+app.post('/user', (req, res) => { 
+    // console.log(req.body)  
+    users.push(req.body) 
+    res.json({ message: "User Added Success" }) 
+}) 
+ 
+// READ - Get All Users  
+ 
+app.get('/user', (req, res) => { 
+    res.json(users) 
+}) 
+ 
+// Get Single User    
+ 
+app.get('/user/:id', (req, res) => { 
+    let id = +req.params.id 
+    let item = users.find((user) => user.id === id) 
+    res.json(item) 
+}) 
+ 
+// Replace Data - Put 
+app.put("/user/:id" , (req,res) => { 
+    let id = +req.params.id 
+    let userIndex = users.findIndex((item) => item.id === id) 
+    users.splice(userIndex, 1 , req.body) 
+    res.json({message: "User Replace Success" })
+}) 
+ 
+// Update data - PATCh 
+ 
+app.patch("/user/:id" , (req,res) => { 
+    let id = +req.params.id 
+    let userIndex = users.findIndex((item) => item.id === id)
+    let user = users[userIndex]
+    users.splice(userIndex , 1 , {...user , ...req.body}) 
+    res.json({message: "User Update Success" })
+}) 
+ 
+// Delete Data - DELETE 
+app.delete("/user/:id" , (req,res) => { 
+    let id = +req.params.id 
+    let userIndex = users.findIndex((item) => item.id === id) 
+    users.splice(userIndex , 1)
+    res.json({message: "User Delete Success" })
+}) 
+ 
+app.listen(1111, () => { 
+    console.log("Server Start At http://localhost:1111") 
 })
-
-server.get('/', (req, res) => {
-    res.write('Get Method - 1');
-    res.end();
-})
-
-server.post('/', (req, res) => {
-    res.write('Welcome to Post Method');
-    res.send('Welcome to Post Method');
-})
-
-server.put('/', (req, res) => {
-    res.json({msg:'Hello Put Method Called'});
-})
-
-server.patch('/', (req, res) => {
-    res.status(400);
-    res.json({msg:'Hello Patch Method Called'});
-})
-
-// server.get('/user', (req, res) => {
-//     res.json(JSON.parse(data));
-// })
-
-server.get('/login', (req, res) => {
-    res.json({msg:'Welcome to Login Page'});
-    res.end();
-})
-
-
-server.listen(2222, () => {
-    console.log('Server Start at http://localhost2222');
-});
