63c1e25377a0287cd8720d470a9a414ddea71ca3
annigodhani <aniruddhgodhani@gmail.com>
2024-08-21 09:48:37 +0530

    21/08/24

---
+++ model/product.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+    pName: {
+        type: String
+    },
+    pDescrip: {
+        type: String
+    },
+    pRate: {
+        type: Number
+    },
+    pWarranty: {
+        type: Number
+    },
+    isDelete: {
+        type: Boolean,
+        default: false
+    }
+},
+    {
+        versionKey: false,
+        timestamps: true
+    })
+
+
+module.exports = mongoose.model('products', productSchema)
---
+++ routes/product.routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const productRoutes = express.Router()
+
+const {
+    addNewUserPro,
+    getAllUsersPro,
+    getUserPro,
+    updateUserPro,
+    deleteUserPro
+} = require('../Controller/product.controller')
+
+productRoutes.post('/', addNewUserPro)
+
+productRoutes.get('/', getAllUsersPro)
+
+productRoutes.get('/productget', getUserPro)
+
+productRoutes.patch("/:id", updateUserPro)
+
+productRoutes.delete("/:id", deleteUserPro)
+
+
+
+module.exports = productRoutes
